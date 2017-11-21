19959eeda4801c92bad1e15d6d10754fda657bf8
杨振 <yangzhennupt@163.com>
2017-11-21 10:24:31 +0800

    整合项目，去除无用东西

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuex from 'vuex';
+import iView from 'iview';
+import 'iview/dist/styles/iview.css';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(Vuex);
+Vue.use(iView);
 
 /* eslint-disable no-new */
 new Vue({
