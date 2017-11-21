f72fc376f4514379a7f85b37655b427ba34d5a22
杨振 <yangzhennupt@163.com>
2017-11-21 10:41:01 +0800

    目录调整

---
+++ src/page/frame.vue
@@ -0,0 +1,15 @@
+<template>
+
+</template>
+
+<script>
+default export{
+    data(){
+        return {
+            msg:"yangz is god"
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss"></style>
