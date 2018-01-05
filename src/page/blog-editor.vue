f23d9e05f8b33199f01cd26dbb9a7523a6bb53a5
杨振 <yangzhennupt@163.com>
2018-01-05 15:05:18 +0800

    初步完成新增博客

--- src/page/blog-editor.vue
+++ src/page/blog-editor.vue
@@ -1,14 +1,45 @@
 <template>
   <div id="blogEditor">
-      <mavon-editor v-model="value"/>
+    <Form :model="blog" label-position="left" >
+         <FormItem label="请输入文章的标题：">
+            <Input v-model="blog.title"></Input>
+        </FormItem>       
+    </Form>
+    <mavon-editor v-model="blog.value"/>    
+    <Button type="primary" @click="submitText" class="saveBtn">保存</Button>
   </div>
+    
 </template>
 
 <script>
 export default {
   data(){
       return {
-          value:''
+          blog:{
+              title:'',
+              value:''
+          }
+      }
+  },
+  methods:{
+      submitText(){
+          var self = this;
+          if(this.blog.title&&this.blog.value){
+                this.$Spin.show();
+                this.$axios.post('/api/blog',{
+                    blog_title:self.blog.title,
+                    blog_content:self.blog.value
+                }).then(res=>{
+                    self.$Message.success('博客保存成功！');
+                    self.$Spin.hide();
+                }).catch(err=>{
+                    self.$Message.error('博客保存失败，请稍后再试。');
+                    self.$Spin.hide();
+                })
+          }else{
+                self.$Message.warning('博客标题或内容不能为空！'); 
+          }
+          
       }
   },
   computed:{
@@ -20,7 +51,12 @@ export default {
 }
 </script>
 
-<style lang="sass" scoped>
-
+<style  scoped>
+#blogEditor{
+    padding: 20px;
+}
+.saveBtn{
+    margin-top: 15px;
+}
 </style>
 
