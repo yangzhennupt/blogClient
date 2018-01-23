422ea5c520d75b58b8abb37a9e6ce319a1063847
杨振 <yangzhennupt@163.com>
2018-01-23 20:09:34 +0800

    提交博客详情组件

---
+++ src/page/blog-detail.vue
@@ -0,0 +1,26 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+
+        }
+    },
+    computed:{
+
+    },
+    created(){
+
+    },
+    methods:{
+        
+    }
+
+}
+</script>
+<style scoped>
+
+</style>
