162d85e7e5d9104f7d2d3a0b12e5225d773aee7f
杨振 <yangzhennupt@163.com>
2018-01-31 17:21:12 +0800

    渲染没问题了

--- src/page/blog-detail.vue
+++ src/page/blog-detail.vue
@@ -9,27 +9,20 @@ export default {
     data(){
         return {
             blog:{
-              blog_content:  null
+              blog_content:  '# test #'
             }
         }
     },
     computed:{     
 		 transArticle(){			 
-			//  for(var i = 0;i<this.article.length;i++){
-			// 	this.article[i].blog_content=this.$marked(this.article[i].blog_content);
-			//  }
-             // return this.article;
-             console.log('计算属性')
-             console.log(this.blog)
              return this.$marked(this.blog.blog_content);
 		 }
     },
-    beforeCreate(){
+    created(){
         let self = this,
             id = this.$route.params.id;
             this.$Spin.show();
 			this.$axios.get('/api/blog/detail/'+id).then(res=>{
-                console.log('赋值')
 				self.blog  = res.data;
 				this.$Spin.hide();
 			}).catch(err=>{
